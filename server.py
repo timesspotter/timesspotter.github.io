fec4f18c456f27eebb4a3bd75317863b6acba9c2
timesspotter <74121755+timesspotter@users.noreply.github.com>
2021-12-26 17:52:00 +0000

    static srver

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
+@app.route("/api/subscribe", methods=["POST"])
+def hello_world():
+    print(f"Message received {request.form}")
+    # Here to send email
+    # return redirect('/static/index.html') 
+
+
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8888)
